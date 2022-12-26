83bb22cd00ea36a531881ec89aebd7312e92ed5c
agoodson582 <agoodson582@gmail.com>
2022-12-26 10:27:12 -0500

    Fixed ball stuttering when hitting side of paddle

--- script.js
+++ script.js
@@ -208,8 +208,8 @@ window.addEventListener('load', () => {
             return (
                 rect1.position.x < rect2.position.x + rect2.size.x &&
                 rect1.position.x + rect1.size.x > rect2.position.x &&
-                rect1.position.y < rect2.position.y + rect2.size.y &&
-                rect1.position.y + rect1.size.y > rect2.position.y
+                rect1.position.y + rect1.size.y > rect2.position.y &&
+                rect1.position.y + rect1.size.y - rect2.position.y <= 5
             )
         }
 
